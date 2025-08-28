e9df6de281b00702e4e4d28712991cbb4663fa5f
Ethan B <e.boulard07@gmail.com>
2025-08-28 11:58:43 +0200

    login

--- src/app/login-form/login-form.ts
+++ src/app/login-form/login-form.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './login-form.html',
   styleUrl: './login-form.css'
 })
 export class LoginForm {
 
+  email!: string;
+  password!: string;
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value); // { username: '...', password: '...' }
+    }
+  }
+
+  sendContact(){
+
+  }
 }
